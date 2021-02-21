958e78c60ce00000f5aec439562f67264cff339b
gmork2 <gmork.02@gmail.com>
2021-02-21 03:21:30 +0100

    Added config file to gulpfile.js

---
+++ gulpfile.js/config.js
@@ -0,0 +1,20 @@
+module.exports = Object.freeze({
+    SRC:         './src/**/*',
+    SRC_HTML:    './src/*.html',
+    SRC_CSS:     './src/**/*.css',
+    SRC_IMG:     './src/**/img/**/*',
+    SRC_JS:      './src/**/*.js',
+
+    BUILD:       '../build',
+    BUILD_INDEX: '../build/index.html',
+    BUILD_CSS:   '../build/**/*.css',
+    BUILD_IMG:   '../build/**/img/**/*',
+    BUILD_JS:    '../build/**/*.js',
+    BUILD_FILES: ['../build/*.html'],
+
+    DIST:        '../dist',
+    DIST_INDEX:  '../dist/index.html',
+    DIST_CSS:    '../dist/**/*.css',
+    DIST_IMG:    '../dist/**/img/**/*',
+    DIST_JS:     '../dist/**/*.js',
+});
