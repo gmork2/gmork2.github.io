204acb631f2f36abba6ffcf7389858a13f6290b0
gmork2 <gmork.02@gmail.com>
2021-02-22 23:28:03 +0100

    Changed <BUILD> options in config

--- gulpfile.js/config.js
+++ gulpfile.js/config.js
@@ -5,12 +5,12 @@ module.exports = Object.freeze({
     SRC_IMG:     './src/**/img/**/*',
     SRC_JS:      './src/**/*.js',
 
-    BUILD:       '../build',
-    BUILD_INDEX: '../build/index.html',
-    BUILD_CSS:   '../build/**/*.css',
-    BUILD_IMG:   '../build/**/img/**/*',
-    BUILD_JS:    '../build/**/*.js',
-    BUILD_FILES: ['../build/*.html'],
+    BUILD:       './build',
+    BUILD_INDEX: './build/index.html',
+    BUILD_CSS:   './build/**/*.css',
+    BUILD_IMG:   './build/**/img/**/*',
+    BUILD_JS:    './build/**/*.js',
+    BUILD_HTML:  './build/*.html',
 
     DIST:        '../dist',
     DIST_INDEX:  '../dist/index.html',
